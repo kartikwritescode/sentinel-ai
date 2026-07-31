fdbc27bc23c3404cba20494abb9f28353facd2da
Kartik Kumar <work.kartikkumar@gmail.com>
2026-07-31 23:38:43 +0530

    config

--- config.py
+++ config.py
@@ -22,3 +22,18 @@ TIER2_MODEL_PATH = 'models/tier2_gru.pt'
 TIER2_INPUT_SIZE = 18              # number of engineered features (you'll tune this)
 TIER2_HIDDEN_SIZE = 64
 TIER2_NUM_LAYERS  = 2
+
+
+# alerting
+
+ALERT_DEBOUNCE_WINDOWS = 3         # need 3 consecutive suspicious windows to fire
+ALERT_CONFIDENCE_THRESH = 0.7     # minimum model confidence to even count a window
+PRE_EVENT_BUFFER_SECONDS = 5      # seconds of footage to save BEFORE the alert
+POST_EVENT_RECORD_SECONDS = 3     # seconds AFTER alert trigger to keep recording
+
+SQLITE_DB_PATH = 'data/events.db'
+EVIDENCE_CLIPS_DIR = 'data/evidence_clips'
+
+# Telegram 
+TELEGRAM_BOT_TOKEN = ''   
+TELEGRAM_CHAT_ID   = ''   
